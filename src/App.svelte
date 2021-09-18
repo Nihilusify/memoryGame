458d614620d6089a552f812dcf7b4d930932fcf0
Terence LeRoy van Jaarsveldt <tvjaarsveldt@meditech.co.za>
2021-09-18 22:19:14 +0200

    basic layout and header

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<header>
+  <h1>Memory Game</h1>
+  <p>Created by Terence</p>
+</header>
+
+<div class="container">
+  <div id="score" class="card">Score</div>
+
+  <div id="board" class="card">Board</div>
+</div>
+
+<style>
+  header {
+    padding: 15px;
+    text-align: center;
+    background: hsl(215, 26%, 23%);
+    font-size: 30px;
+  }
+  .container {
+    width: 80%;
+    margin: 5vh auto;
+  }
+</style>
